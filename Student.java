de26923e42450aafdd27bec270c430596b23b12a
vaishnavipawar25 <vaishnavipawar875@gmail.com>
2024-12-11 22:25:59 +0530

    Student class added

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public Student()
+    {
+        System.out.println("creating student obj");
+    }
+    public void sayHello()
+    {
+        System.out.println("helloo");
+    }
+}
